171e9a685421ed32044cf94bc9fb9fa4d587052d
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-12-28 23:27:45 -0300

    added axios interceptor

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,7 +3,30 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
   timeout: 10000,
+  withCredentials: false,
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log("Full URL:", `${config.baseURL ?? ""}${config.url}`);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log("Response:", response);
+    return response;
+  },
+  (error) => {
+    console.log("Error:", error.config);
+    return Promise.reject(error);
+  }
+);
